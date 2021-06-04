8b925e86a7174e04b0ceb8e9b00252c20e133e0e
linqiankai <417350372@qq.com>
2021-06-04 11:41:55 +0800

    usage添加

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -13,10 +14,10 @@ var (
 
 func usage() {
 	_, _ = fmt.Fprintf(os.Stderr, "Usage of router-gen:\n")
-	_, _ = fmt.Fprintf(os.Stderr, "@router: router path\n")
-	_, _ = fmt.Fprintf(os.Stderr, "@method: http method\n")
-	_, _ = fmt.Fprintf(os.Stderr, "@group: router group\n")
-	_, _ = fmt.Fprintf(os.Stderr, "only support github.com/gin-gonic/gin\n")
+	_, _ = fmt.Fprintf(os.Stderr, "	@router: router path\n")
+	_, _ = fmt.Fprintf(os.Stderr, "	@method: http method\n")
+	_, _ = fmt.Fprintf(os.Stderr, "	@group: router group\n")
+	_, _ = fmt.Fprintf(os.Stderr, "	only support github.com/gin-gonic/gin\n")
 	flag.PrintDefaults()
 }
 
@@ -25,11 +26,13 @@ func main() {
 	flag.Parse()
 	_, err := os.Stat(*dir)
 	if err != nil {
+		log.Println(err)
 		flag.Usage()
 		os.Exit(2)
 	}
 	_, err = os.Stat(*output)
 	if err != nil {
+		log.Println(err)
 		flag.Usage()
 		os.Exit(2)
 	}
